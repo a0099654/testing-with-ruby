d8f01662a325e0e52e4f1df0d25731d237b81880
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-22 18:02:05 +0800

    [Stella]Implement methods for rendering achivement on the public page

---
+++ spec/features/achievement_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'scenario page' do
+    scenario 'achievement public page' do
+        achievement = Achievement.create(title: 'Just did it')
+        visit("/achievements/#{achievement.id}")
+
+        expect(page).to have_content('Just did it ')
+    end
+end
+
