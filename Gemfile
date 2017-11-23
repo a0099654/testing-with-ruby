b73a448e53afe8123ba99da17af72cca90d80079
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-23 11:12:00 +0800

    [Stella]Adding method to test on the markdown description on the form

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'jquery-rails'
 # Download simple form
 gem 'simple_form'
 
+# Process markdown
+gem 'redcarpet'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- spec/features/achievement_page_spec.rb
+++ spec/features/achievement_page_spec.rb
@@ -16,7 +16,7 @@ feature 'scenario page' do
         achievement = FactoryBot.create(:achievement, description: 'That *was* hard')
         visit("/achievements/#{achievement.id}")
 
-        expect(page).to have_content('<i>was</i>') 
+        expect(page).to have_css('em', text:'was') 
     end
 end
 
