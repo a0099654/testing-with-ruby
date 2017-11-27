a80299b4f1aab18daeeea574e00c2c88fedd9b66
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-27 12:36:27 +0800

    [Stella]Create a login form and refactor capybara test

--- spec/features/achievement_page_spec.rb
+++ spec/features/achievement_page_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 feature 'scenario page' do
-    # scenario 'achievement public page' do
-    #     achievement = FactoryBot.create(:achievement, title: 'Just did it')
-    #     visit("/achievements/#{achievement.id}")
+    scenario 'achievement public page' do
+        achievement = FactoryBot.create(:achievement, title: 'Just did it')
+        visit("/achievements/#{achievement.id}")
 
-    #     expect(page).to have_content('Just did it ')
+        expect(page).to have_content('Just did it ')
 
-    #     achievements = FactoryBot.create_list(:achievement, 3)
-    #     # p achievements
+        achievements = FactoryBot.create_list(:achievement, 3)
+        # p achievements
 
-    # end 
+    end 
 
-    # scenario 'render markdown description' do
-    #     achievement = FactoryBot.create(:achievement, description: 'That *was* hard')
-    #     visit("/achievements/#{achievement.id}")
+    scenario 'render markdown description' do
+        achievement = FactoryBot.create(:achievement, description: 'That *was* hard')
+        visit("/achievements/#{achievement.id}")
 
-    #     expect(page).to have_css('em', text:'was') 
-    # end
+        expect(page).to have_css('em', text:'was') 
+    end
 end
 
