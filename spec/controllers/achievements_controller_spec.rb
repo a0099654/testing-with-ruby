66653daa914a192c1f033f7c935331db36c006cd
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-27 11:38:02 +0800

    [Stella]Implement testing for any authenticated user

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -68,7 +68,77 @@ describe AchievementsController do
         end
         
     end
-   
+
+    describe 'authenticated user' do
+        let(:user) { FactoryBot.create(:user) }
+        before do
+            sign_in(user)
+        end
+        describe 'GET index' do
+            it 'renders :index template' do
+                get :index
+                expect(response).to render_template(:index)
+            end
+    
+            it 'assigns only public achievements to the template' do
+                public_achievement = FactoryBot.create(:public_achievement)
+                private_achievement = FactoryBot.create(:private_achievement)
+    
+                get :index
+                expect(assigns(:achievements)).to match_array([public_achievement])
+            end
+        end
+
+        describe 'GET show' do
+            let(:achievement) { FactoryBot.create(:public_achievement)}
+    
+            it 'renders :show template' do
+                get :show, params: { id: achievement }
+                expect(response).to render_template(:show)
+            end
+            it 'assigns requested achievement to @achievement' do
+                get :show, params: { id: achievement }
+                expect(assigns(:achievement)).to eq(achievement)
+            end
+        end
+
+        describe 'GET new' do
+            it 'renders :new template' do
+                get :new
+                expect(response).to render_template(:new)
+            end
+
+            it 'assigns new achievement to @achievement' do
+                get :new
+                expect(assigns(:achievement)).to be_a_new(Achievement)
+            end
+        end
+
+        context 'is not the owner of the achievement' do
+            describe 'GET edit' do
+                it 'redirects to achievements page' do
+                    get :edit,  params: { id: FactoryBot.create(:public_achievement) } 
+                    expect(response).to redirect_to(achievement_path)
+                end
+            end
+    
+            describe 'PUT update' do
+                it 'redirects to achievements page' do
+                    put :update, params: { id: FactoryBot.create(:public_achievement), achievement: FactoryBot.attributes_for(:public_achievement) }
+                    expect(response).to redirect_to(achievement_path)
+                end
+            end
+    
+            describe 'DELETE destroy' do
+                it 'redirects to achievements page' do
+                    delete :destroy, params: { id: FactoryBot.create(:public_achievement) }    
+                    expect(response).to redirect_to(achievement_path)
+                end
+            end
+        end
+
+        context 'is the owner of the achievement' 
+    end
 
     # describe 'GET edit' do
     #     let(:achievement) { FactoryBot.create(:public_achievement) }
@@ -80,18 +150,6 @@ describe AchievementsController do
     #     it 'assigns the request achievement to template' do
     #         get :edit, params: { id: achievement} 
     #         expect(assigns(:achievement)).to eq(achievement)
-    #     end
-    # end
-
-    # describe 'GET new' do
-    #     it 'renders :new template' do
-    #         get :new
-    #         expect(response).to render_template(:new)
-    #     end
-
-    #     it 'assigns new achievement to @achievement' do
-    #           get :new
-    #         expect(assigns(:achievement)).to be_a_new(Achievement)
     #     end
     # end
 
