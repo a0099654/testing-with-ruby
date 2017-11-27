458d60c9418e66391587ec38c012ca98af8bfb99
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-27 17:20:19 +0800

    [Stella]Making a custom built-in validation and using shoulda matchers to make the tests shorter

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -4,21 +4,9 @@ RSpec.describe Achievement, type: :model do
   describe 'validation' do
 
     it { should validate_presence_of(:title) }
-
-    it 'requires title to be unique for one user' do
-      user = FactoryBot.create(:user)
-      first_achievement  = FactoryBot.create(:public_achievement, title: 'First Achievement', user: user)
-      new_achievement =  Achievement.new(title:  'First Achievement', user: user)
-      expect(new_achievement.valid?).to be_falsy
-    end
-
-    it 'allows different users to have achievements with identical titles' do
-      user1 = FactoryBot.create(:user)
-      user2 = FactoryBot.create(:user)
-      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achievement', user: user1)
-      new_achievement =  Achievement.new(title:  'First Achievement', user: user2)
-      expect(new_achievement.valid?).to be_truthy
-    end
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("you can't have two achievements with the same title") }
+    # it { should validate_presence_of(:user) } user's presence supposed to be true and not false
+    it { should belong_to(:user)}
   end
 
   it 'belongs to user' do
@@ -26,5 +14,4 @@ RSpec.describe Achievement, type: :model do
     expect(achievement.valid?).to be_truthy
   end
 
-  it { should belong_to(:user)}
 end
