7cebad23aa63c2542da43ec4ad6a9415c30b2e20
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-27 12:47:58 +0800

    [Stella]Allowing only 1 assertion for achievement model validation method

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe Achievement, type: :model do
   describe 'validation' do
     it 'requires title' do
       achievement = Achievement.new(title: '')
-      achievement.valid?
-      expect(achievement.errors[:title]).to include("can't be blank")
-      expect(achievement.errors[:title]).not_to be_empty
       expect(achievement.valid?).to be_falsy
     end
   end
