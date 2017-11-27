216fa0c3ccc947b7ab20313d7b14742faa48416b
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-27 15:13:02 +0800

    [Stella]Implement method to check that different user can have identical titles

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,11 +2,19 @@ class Achievement < ApplicationRecord
     belongs_to :user, optional: true #association
       
     validates :title, presence: true #validation
-    validates :title, uniqueness: true
+    # validates :title, uniqueness: true
+    validate :unique_title_for_one_user
 
     enum privacy: [:public_access, :private_access, :friends_access]
 
     def description_html
         Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+    end
+
+    def unique_title_for_one_user
+        existing_achievement = Achievement.find_by(title: title)
+        if existing_achievement && existing_achievement.user == user
+            errors.add(:title, "you can't have two achievements with the same title")
+        end
     end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -14,5 +14,12 @@ RSpec.describe Achievement, type: :model do
       expect(new_achievement.valid?).to be_falsy
     end
 
+    it 'allows different users to have achievements with identical titles' do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achievement', user: user1)
+      new_achievement =  Achievement.new(title:  'First Achievement', user: user2)
+      expect(new_achievement.valid?).to be_truthy
+    end
   end
 end
