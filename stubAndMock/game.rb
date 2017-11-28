9e23a8b878d9fbc3d4441b1fe54b57b0c9555f35
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-28 10:31:41 +0800

    [Stella]Adding mock to test logger

--- stubAndMock/game.rb
+++ stubAndMock/game.rb
@@ -1,35 +1,47 @@
 class Character
-    def initialize(strength: 1, die: Die.new)
+    def initialize(strength: 1, die: Die.new, logger: Logger.new)
         @strength = strength
         @die = die
+        @logger = logger
     end 
 
     def climb(difficulty: 10)
-        die.roll + strength >= difficulty
+        roll = die.roll + strength 
+        logger.log("Climbing check. Difficulty:#{difficulty}. Roll:#{roll}")
+        roll >= difficulty
     end
 
     private
 
-    attr_reader :strength, :die
+    attr_reader :die, :logger, :strength
 end
  
 describe Character do
     describe 'climbing check skill' do
         let(:die) { double }  
-        let(:character) { Character.new(strength: 5, die: die) }
+        let(:logger) { double }
+        let(:character) { Character.new(strength: 5, die: die, logger: logger) }
 
         #stub
         it 'climbs successfully when roll + strength is more than difficulty' do
             allow(die).to receive(:roll) { 11 }
+            allow(logger).to receive(:log)
             expect(character.climb(difficulty: 15)).to be_truthy
         end
 
         #stub
         it 'fails climbing check when roll + strength is length than difficulty' do
             allow(die).to receive(:roll) { 5 }
+            allow(logger).to receive(:log)
             expect(character.climb(difficulty: 15 )).to be_falsy
         end
 
+        #mock 
+        it 'commands logger to log climb skill check' do
+            allow(die).to receive(:roll) { 7 }
+            expect(logger).to receive(:log).with('Climbing check. Difficulty:10. Roll:12')
+            character.climb(difficulty: 10)
+        end
 
     end
 end
