970675526d0750cc448da3d043b0ab2fa9d6947e
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-28 16:13:02 +0800

    [Stella]Implement testing for the body of the email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,7 +1,9 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  let(:email) { email = UserMailer.achievement_created('author@gmail.com').deliver_now }
+
+  let(:achievement_id) {  1 }
+  let(:email) { email = UserMailer.achievement_created('author@gmail.com', achievement_id).deliver_now }
   
   it 'sends achievement created email to author' do
     expect(email.to).to include('author@gmail.com')
@@ -9,5 +11,9 @@ RSpec.describe UserMailer, type: :mailer do
 
   it 'has a correct subject' do
     expect(email.subject).to eq('Congratulations with your new achievement')
+  end
+
+  it 'has achievement link in body message' do
+    expect(email.body.to_s).to include(achievement_url(achievement_id))
   end
 end
