6d43db7c7a786df1a694db9359e6480b1f634968
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-28 10:18:07 +0800

    [Stella]Implement stub in the game class

--- stubAndMock/game.rb
+++ stubAndMock/game.rb
@@ -7,16 +7,29 @@ class Character
     def climb(difficulty: 10)
         die.roll + strength >= difficulty
     end
+
+    private
+
+    attr_reader :strength, :die
 end
  
 describe Character do
     describe 'climbing check skill' do
         let(:die) { double }  
         let(:character) { Character.new(strength: 5, die: die) }
+
+        #stub
         it 'climbs successfully when roll + strength is more than difficulty' do
+            allow(die).to receive(:roll) { 11 }
             expect(character.climb(difficulty: 15)).to be_truthy
         end
 
-        it 'fails climbing check when roll + strength is length than difficulty'
+        #stub
+        it 'fails climbing check when roll + strength is length than difficulty' do
+            allow(die).to receive(:roll) { 5 }
+            expect(character.climb(difficulty: 15 )).to be_falsy
+        end
+
+
     end
 end
