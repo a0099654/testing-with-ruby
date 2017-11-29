df024d028ec27ef421fd8c6fc98ab899caf0a742
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-29 16:37:38 +0800

    [Stella]Adding vcr and webmock for testing the third party API of the application

--- Gemfile
+++ Gemfile
@@ -72,6 +72,8 @@ group :test do
   # database_cleaner is not required, but highly recommended
   gem 'database_cleaner'
   gem 'shoulda-matchers', :require => false
+  gem 'vcr'
+  gem 'webmock'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'devise'
+require 'vcr'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,6 +60,12 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::TestHelpers, type: :controller
+  
+  VCR.configure do |c|
+    c.cassette_library_dir = 'spec/cassettes'
+    c.hook_into :webmock
+    c.allow_http_connections_when_no_cassette = true
+  end
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
