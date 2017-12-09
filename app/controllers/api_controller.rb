9379755323ce8e87b0f101b86a82db24f5cd8f46
Stella <stella.widyasari@gmail.com>
2017-12-09 11:45:27 +0800

    [Stella]Adding validateheader->still not working

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,15 @@
 class ApiController < ActionController::Base
     protect_from_forgery with: :null_session
-  end
+
+    # before_action :validate_header
+
+    private
+
+    # def validate_header
+    #   if request.headers["Content-Type"] != 'application/vnd.api+json'
+    #     render json: {}, status: 200
+    #     return 
+    #   end
+    # end
+end
   
